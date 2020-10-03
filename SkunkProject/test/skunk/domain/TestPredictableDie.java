27845ce2af943236ddeb0054ad125117b31589fb
Riley Martinez <riley.m.martinez@gmail.com>
2020-10-03 15:20:18 -0500

    Write initial failing test test_predictable_die_1_2_3

---
+++ SkunkProject/test/skunk/domain/TestPredictableDie.java
@@ -0,0 +1,14 @@
+package skunk.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPredictableDie {
+
+	@Test
+	public void test_predictable_die_1_2_3() {
+		fail("Not yet implemented");
+	}
+
+}
