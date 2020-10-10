8d0bf2d422147f9ee088341d5ed9e18ec7fa1f54
Riley Martinez <riley.m.martinez@gmail.com>
2020-10-10 17:14:15 -0500

    Create 1st initial failed test for PredictableDice

--- SkunkProject/test/skunk/domain/TestPredictableDice.java
+++ SkunkProject/test/skunk/domain/TestPredictableDice.java
@@ -1,5 +1,13 @@
 package skunk.domain;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TestPredictableDice {
 
+	@Test
+	public void test_predictable_dice_1_2_3() {
+		fail();
+	}
 }
