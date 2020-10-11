9efe7bd4b1e4d4ea45240cb1709e6337dab13b23
Riley Martinez <riley.m.martinez@gmail.com>
2020-10-10 23:38:10 -0500

    Create initial 1st failing TestDice test

--- SkunkProject/test/skunk/domain/TestDice.java
+++ SkunkProject/test/skunk/domain/TestDice.java
@@ -1,5 +1,13 @@
 package skunk.domain;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestDice {
 
+	@Test
+	public void test_dice_skunk_roll() {
+		fail("Not yet implemented.");
+	}
 }
