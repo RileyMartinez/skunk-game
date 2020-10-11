b6850db759f460ba8a5164b914dd00ffbbc2746e
Riley Martinez <riley.m.martinez@gmail.com>
2020-10-10 23:24:27 -0500

    Create initial 2nd failing test for TestDie

--- SkunkProject/test/skunk/domain/TestDie.java
+++ SkunkProject/test/skunk/domain/TestDie.java
@@ -7,12 +7,17 @@ import org.junit.Test;
 public class TestDie {
 
 	@Test
-	public void test_valid_roll_range_with_100_rolls() {
+	public void test_die_valid_roll_range_with_100_rolls() {
 		Die die = new Die();
 		for (int i = 0; i < 100; i++) {
 			die.roll();
 			assertTrue(die.getLastRoll() >= 1 && die.getLastRoll() <= 6);
 		}
+	}
+	
+	@Test
+	public void test_die_to_string() {
+		fail("Not yet implemented.");
 	}
 
 }
