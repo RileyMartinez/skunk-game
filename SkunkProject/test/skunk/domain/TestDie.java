0b4ae80533c0ae243ea06e9ac0efb99c7a5cb2fd
Riley Martinez <riley.m.martinez@gmail.com>
2020-10-10 23:19:32 -0500

    Make 1st TestDie test pass

--- SkunkProject/test/skunk/domain/TestDie.java
+++ SkunkProject/test/skunk/domain/TestDie.java
@@ -7,8 +7,12 @@ import org.junit.Test;
 public class TestDie {
 
 	@Test
-	public void test_valid_roll_range() {
-		fail("Not yet implemented");
+	public void test_valid_roll_range_with_100_rolls() {
+		Die die = new Die();
+		for (int i = 0; i < 100; i++) {
+			die.roll();
+			assertTrue(die.getLastRoll() >= 1 && die.getLastRoll() <= 6);
+		}
 	}
 
 }
