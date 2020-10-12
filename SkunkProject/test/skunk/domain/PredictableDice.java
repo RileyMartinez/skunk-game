aec4c1e32672b5078fa52f8f2be859fb351d10e9
Riley Martinez <riley.m.martinez@gmail.com>
2020-10-12 00:34:21 -0500

    Refactor PredictableDice and create 8th failing TestPredictableDice test

--- SkunkProject/test/skunk/domain/PredictableDice.java
+++ SkunkProject/test/skunk/domain/PredictableDice.java
@@ -1,46 +1,39 @@
 package skunk.domain;
 
 public class PredictableDice {
-	private int[] ceilingRoll;
-	private int[] floorRoll;
 	private int lastRoll;
-	private PredictableDie ceilingDie;
-	private PredictableDie floorDie;
+	private PredictableDie die1;
+	private PredictableDie die2;
 	private boolean isSkunk;
 	private boolean isDeuce;
 	
 	
-	public PredictableDice(int[] is) {
-		if (is.length == 0) {
-			throw new RuntimeException("No roll value in array.");
+	public PredictableDice(int[] die1Rolls, int[] die2Rolls) {
+		if (die1Rolls.length == 0 || die2Rolls.length == 0) {
+			throw new RuntimeException("No roll values in array.");
 		}
-		ceilingRoll = new int[is.length];
-		floorRoll = new int[is.length];
-		
-		for (int i = 0; i < is.length; i++) {
-			if (is[i] < 2) {
+		for (int i = 0; i < die1Rolls.length; i++) {
+			if (die1Rolls[i] + die2Rolls[i] < 2) {
 				throw new RuntimeException("Dice can't roll a value less than 2.");
-			} else if (is[i] > 12) {
+			} else if (die1Rolls[i] + die2Rolls[i] > 12) {
 				throw new RuntimeException("Dice can't roll a value greater than 12.");
 			}
-			ceilingRoll[i] = (int)Math.ceil((double)is[i]/2);
-			floorRoll[i] = (int)Math.floor((double)is[i]/2);
 		}
-		ceilingDie = new PredictableDie(ceilingRoll);
-		floorDie = new PredictableDie(floorRoll);
+		die1 = new PredictableDie(die1Rolls);
+		die2 = new PredictableDie(die2Rolls);
 	}
 	
 	public void roll() {
-		ceilingDie.roll();
-		floorDie.roll();
-		this.lastRoll = ceilingDie.getLastRoll() + floorDie.getLastRoll();
+		die1.roll();
+		die2.roll();
+		this.lastRoll = die1.getLastRoll() + die2.getLastRoll();
 		checkLastRoll();
 	}
 	
 	public void checkLastRoll() {
-		if (ceilingDie.getLastRoll() == 1 || floorDie.getLastRoll() == 1) {
+		if (die1.getLastRoll() == 1 || die2.getLastRoll() == 1) {
 			this.isSkunk = true;
-			if (ceilingDie.getLastRoll() == 1 && floorDie.getLastRoll() == 1) {
+			if (die1.getLastRoll() == 1 && die2.getLastRoll() == 1) {
 				this.isDeuce = true;
 			} else {
 				this.isDeuce = false;
--- SkunkProject/test/skunk/domain/TestPredictableDice.java
+++ SkunkProject/test/skunk/domain/TestPredictableDice.java
@@ -8,52 +8,77 @@ public class TestPredictableDice {
 
 	@Test
 	public void test_predictable_dice_roll_range() {
-		int[] testRolls = new int[] {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; 
-		PredictableDice dice = new PredictableDice(testRolls);
-		for (int i = 0; i < testRolls.length; i++) {
+		int[] die1Rolls = {
+				1, 1, 1, 1, 1, 1,
+				2, 2, 2, 2, 2, 2,
+				3, 3, 3, 3, 3, 3,
+				4, 4, 4, 4, 4, 4,
+				5, 5, 5, 5, 5, 5,
+				6, 6, 6, 6, 6, 6
+		};
+		
+		int[] die2Rolls = {
+				1, 2, 3, 4, 5, 6,
+				1, 2, 3, 4, 5, 6,
+				1, 2, 3, 4, 5, 6,
+				1, 2, 3, 4, 5, 6,
+				1, 2, 3, 4, 5, 6,
+				1, 2, 3, 4, 5, 6
+				
+		};
+		
+		PredictableDice dice = new PredictableDice(die1Rolls, die2Rolls);
+		for (int i = 0; i < die1Rolls.length; i++) {
 			dice.roll();
-			assertEquals(testRolls[i], dice.getLastRoll());
+			assertEquals(die1Rolls[i] + die2Rolls[i], dice.getLastRoll());
 		}
 	}
 	
+	
 	@Test
 	public void test_predictable_dice_2_more_than_once() {
-		PredictableDice dice = new PredictableDice(new int[] {2});
+		PredictableDice dice = new PredictableDice(new int[] { 1 }, new int[] { 1 });
 		dice.roll();
 		assertEquals(2, dice.getLastRoll());
 		dice.roll();
 		assertEquals(2, dice.getLastRoll());
 	}
-	
-	@Test (expected = RuntimeException.class)
+
+	@Test(expected = RuntimeException.class)
 	public void test_predictable_dice_with_empty_initial_int_array() {
-		PredictableDice dice = new PredictableDice(new int[] {});
+		PredictableDice dice = new PredictableDice(new int[] {}, new int[] {});
 		dice.roll();
 	}
-	
-	@Test (expected = RuntimeException.class)
+
+	@Test(expected = RuntimeException.class)
 	public void test_predictable_dice_with_value_beyond_lower_bound() {
-		PredictableDice dice = new PredictableDice(new int[] {1});
+		PredictableDice dice = new PredictableDice(new int[] { 0 }, new int[] { 1 });
 		dice.roll();
 	}
-	
-	@Test (expected = RuntimeException.class)
+
+	@Test(expected = RuntimeException.class)
 	public void test_predictable_dice_with_value_beyond_upper_bound() {
-		PredictableDice dice = new PredictableDice(new int[] {13});
+		PredictableDice dice = new PredictableDice(new int[] { 6 }, new int[] { 7 });
 		dice.roll();
 	}
-	
+
 	@Test
 	public void test_predictable_dice_roll_skunk() {
-		PredictableDice dice = new PredictableDice(new int[] {3});
+		PredictableDice dice = new PredictableDice(new int[] { 1 }, new int[] { 5 });
 		dice.roll();
 		assertTrue(dice.getIsSkunk());
 	}
-	
+
 	@Test
 	public void test_predictable_dice_roll_deuce() {
-		PredictableDice dice = new PredictableDice(new int[] {2});
+		PredictableDice dice = new PredictableDice(new int[] { 1 }, new int[] { 1 });
 		dice.roll();
 		assertTrue(dice.getIsDeuce());
 	}
+	
+	@Test(expected = RuntimeException.class)
+	public void test_predictable_dice_with_unequal_number_of_rolls_between_each_die() {
+		fail("Not yet implemented.");
+	}
+	 
 }
