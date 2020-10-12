2daa06fa9c508de200c1eda38263260ce8db9942
icequirrel <nab73@cornell.edu>
2020-10-12 09:16:28 -0500

    JUnit Test class added. testGameCreation() added in invalid state

---
+++ SkunkProject/test/skunk/domain/TestGame.java
@@ -0,0 +1,16 @@
+package skunk.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGame {
+
+	
+	@Test
+	//test to verify that a game is actually instantiated
+	public void testGameCreation() {
+		Game myGame = new Game(); 
+	}
+
+}
