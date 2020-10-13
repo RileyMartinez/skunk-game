bb601ba3a4e6276c179b42c0b82a979e2a3c2508
icequirrel <nab73@cornell.edu>
2020-10-12 20:40:28 -0500

    Added TestTurn class. Included test to see if Turns are instantiated. Added Turn Class

---
+++ SkunkProject/test/skunk/domain/TestTurn.java
@@ -0,0 +1,16 @@
+package skunk.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTurn {
+
+	@Test
+	//Test to see if a turn can be instantiated
+	public void testGameCreation() {
+		Turn myTurn = new Turn();
+		
+	}
+
+}
