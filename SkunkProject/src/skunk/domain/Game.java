babe0fbd3e8523c50d54349bac9553cb408498a4
icequirrel <nab73@cornell.edu>
2020-10-12 20:18:08 -0500

    Refactored the Game.getStatus() method to allow testGameStatus() to pass.

--- SkunkProject/src/skunk/domain/Game.java
+++ SkunkProject/src/skunk/domain/Game.java
@@ -4,14 +4,18 @@ package skunk.domain;
 public class Game {
 
 	boolean isStarted = false; 
+	boolean isCompleted = false;
 
 	
 	public String getStatus() {
-		if(isStarted == false) { 
+		if(isStarted == false && isCompleted == false) { 
 			return "The Game has not started yet";
 		}
-		else {
+		if(isStarted == true && isCompleted == false) {
 			return "The Game is Afoot!";
+		}
+		else {
+			return "The Game has ended";
 		}
 		
 	}
@@ -22,6 +26,7 @@ public class Game {
 	}
 
 	public void completeGame() {
+		this.isCompleted = true;
 		// TODO Auto-generated method stub
 		
 	}
