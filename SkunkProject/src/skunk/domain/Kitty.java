ec2baae17175ef85275a2f38d34c8748b8ba63a4
Riley Martinez <riley.m.martinez@gmail.com>
2020-10-14 02:22:20 -0500

    Make 1st TestKitty test pass

--- SkunkProject/src/skunk/domain/Kitty.java
+++ SkunkProject/src/skunk/domain/Kitty.java
@@ -1,5 +1,19 @@
 package skunk.domain;
 
 public class Kitty {
+	
+	private int chips;
+	
+	public void addChips(int i) {
+		this.setChips(this.getChips() + i);
+	}
+
+	public int getChips() {
+		return chips;
+	}
+
+	public void setChips(int chips) {
+		this.chips = chips;
+	}
 
 }
