5dd3b667fbf40a98fec1907d8c2f4b0bfae6786c
Riley Martinez <riley.m.martinez@gmail.com>
2020-10-14 00:32:55 -0500

    Create "new" 2nd failing test for TestTurn

--- SkunkProject/test/skunk/domain/TestTurn.java
+++ SkunkProject/test/skunk/domain/TestTurn.java
@@ -9,8 +9,15 @@ public class TestTurn {
 	@Test
 	// Create a new turn with an active player.
 	public void test_begin_turn_with_1_player() {
-		Player activePlayer = new Player();
-		Turn currentTurn = new Turn(activePlayer);
+		Player player = new Player();
+		Turn turn = new Turn(player);
 	}
 
+	@Test
+	public void test_increasing_score() {
+		Player player = new Player();
+		Turn turn = new Turn(player);
+		turn.increaseScore(12);
+		assertEquals(turn.getScore(), 12);
+	}
 }
