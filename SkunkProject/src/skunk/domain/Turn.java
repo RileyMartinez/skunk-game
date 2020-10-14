f1364679b5dcbedc9ec42f1ad275f3131d624b05
Riley Martinez <riley.m.martinez@gmail.com>
2020-10-14 00:38:00 -0500

    Make test_increasing_score from TestTurn pass

--- SkunkProject/src/skunk/domain/Turn.java
+++ SkunkProject/src/skunk/domain/Turn.java
@@ -3,8 +3,21 @@ package skunk.domain;
 public class Turn {
 
 	private Player player;
+	private int score;
 
 	public Turn(Player p) {
 		this.player = p;
+	}
+
+	public void increaseScore(int i) {
+		this.setScore(i);
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
 	}
 }
